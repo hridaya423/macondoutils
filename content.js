fe5aa0450e052bf81ff5307619534134b71768d0
Hridya Agrawal <hridayahoney@gmail.com>
2026-05-24 19:08:08 +0100

    initial setup

---
+++ content.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+
+  if (window.__macondoUtilsLoaded) {
+    return;
+  }
+  window.__macondoUtilsLoaded = true;
+
+  console.log("[Macondo Utils] content script loaded");
+})();
